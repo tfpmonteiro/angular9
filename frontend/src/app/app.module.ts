37069db8570b38f7d610d7c38540634104f54af8
Talita Monteiro <tfpmonteiro@outlook.com>
2020-05-26 22:50:08 -0300

    criando uma diretiva estrutural

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,7 +14,8 @@ import {MatCardModule} from '@angular/material/card'
 
 import {HomeComponent} from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { RedDirective } from './directives/red.directive'
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive'
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +24,8 @@ import { RedDirective } from './directives/red.directive'
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  stack = [
+    'Angular 9',
+    'Material',
+    'Json-Service'
+  ];
+
   constructor() { }
 
   ngOnInit() {
